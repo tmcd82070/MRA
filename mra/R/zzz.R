cdf2ee736585ca23d64d74ab73ecdc041eb95a7e
tmcd <tmcdonald@west-inc.com>
2015-03-26 11:19:54 -0600

    Change to startup message in .onAttache()

--- mra/R/zzz.R
+++ mra/R/zzz.R
@@ -3,8 +3,8 @@
 	#library.dynam("mra", pkgname, libname)  # deprecated
     #v <- packageDescription("mra", fields="Version")  # this requires utils package, and I don't want to make mra dependent on utils, just for this.
 
-    packageStartupMessage( "Mark-Recapture Analysis (vers 2.16)" )  # You have to change this every version
-	packageStartupMessage("\ncontact author: Trent McDonald (tmcdonald@west-inc.com)\ncontributions: Eric Regehr, Jeff Bromaghin") 
+    packageStartupMessage( "Mark-Recapture Analysis (vers 2.16.3)" )  # You have to change this every version
+	#packageStartupMessage("\ncontact author: Trent McDonald (tmcdonald@west-inc.com)\ncontributions: Eric Regehr, Jeff Bromaghin") 
 }
 
 
